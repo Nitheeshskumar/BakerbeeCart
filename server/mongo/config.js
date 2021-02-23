0e765383305243f7d16cc0e1726c0e49895a13b4
Nitheesh.S.Kumar <nitheesh.sk@inapp.com>
2021-02-23 15:48:03 +0530

    initial comit

---
+++ server/mongo/config.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const uri = "mongodb+srv://Nitheesh:Nitheesh@clusternitheesh-cbczs.mongodb.net/Nitheeshdb?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+    useNewUrlParser: true
+}); // connect to our database
+
+// db.on('error', console.error.bind(console, 'connection error:'));
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+
+module.exports = mongoose
